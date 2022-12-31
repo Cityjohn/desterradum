d8d510790aa638b1d3e84c4cb71ecd0a6dbd8882
sebasman15 <sebastiansmit1995@gmail.com>
2022-12-31 23:01:43 +0100

    selectie toegevoegd

--- js_timer/timer.js
+++ js_timer/timer.js
@@ -2,7 +2,12 @@ const button = document.getElementById('button');
 const circle = document.getElementById('circle');
 const timer = document.getElementById('timer');
 const select = document.getElementById('form-field-subject');
-const subject = select.value;
+const subject = document.getElementById('subject');
+const $user_id = 0;
+
+
+
+
 
 let time = 0;
 let interval;
@@ -10,21 +15,27 @@ let startTime;
 let stopTime;
 
 button.addEventListener('click', () => {
-  if (interval) {
+  if(subject.value === "Select"){return;}
+  if (interval) 
+  {
     clearInterval(interval);
     interval = null;
     time = 0;
     timer.textContent = 'start';
     stopTime = new Date();
     console.log($user_id, subject, startTime, stopTime);
-  } else {
+  } 
+  else 
+  {
     startTime = new Date();
     timer.textContent = '00:00';
-    interval = setInterval(() => {
+    interval = setInterval(() => 
+    {
       time++;
       const minutes = Math.floor(time / 60);
       const seconds = time % 60;
       timer.textContent = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
     }, 1000);
   }
+  
 });
