01de5310eb3c797a7bfef411c207b5cd0253f51f
sebasman15 <sebastiansmit1995@gmail.com>
2023-03-05 16:51:20 +0100

    working example of connecting to the wp-db with correct prefix

--- plugins/Ajax_test/Ajax_test.php
+++ plugins/Ajax_test/Ajax_test.php
@@ -15,10 +15,30 @@ $password = ".passwordistan";
 $dbname = "testdb_";
 $user_id = get_current_user_id();
 
-// $servername = "edusid.com";
-// $username = "";
-// $password = "";
-// $dbname = "testdb_";
+$servername = "edusid.com";
+$username = "";
+$password = "";
+$dbname = "testdb_";
+
+function connect_to_wp_db()
+{
+  global $wpdb;
+
+  $wpdb->set_prefix('wp_edusid');
+
+  echo '<script>console.log("wp db type: ' . gettype($wpdb) . '")</script>';
+  echo '<script>console.log("wp db: ' . $wpdb->dbname . '")</script>';
+
+  $user_email = $wpdb->get_var("SELECT user_email FROM $wpdb->prefix.edu_users WHERE id = 2");
+  
+  echo '<script>console.log("email var type: ' . gettype($user_email) . '")</script>';
+  echo '<script>console.log("email var type: ' . $user_email . '")</script>';
+
+
+
+  return 0;
+}
+
 
 function connectToDatabase($servername, $username, $password, $dbname) 
 {
@@ -39,13 +59,19 @@ function connectToDatabase($servername, $username, $password, $dbname)
 // check if the page is the timer-pupil page
 function my_page_alert() {
   $user_id = get_current_user_id();
+  $user_email = connect_to_wp_db();
+  
+
   if ( is_page( 'timer-pupil' ) ) 
   {
     ?>
       <script src="<?php echo plugin_dir_url( __FILE__ ) . 'Ajax_test.js'; ?>"></script>
       <script>
         var user_id = <?php echo $user_id; ?>;
-        alert("user id = " + user_id)
+        var user_email = 0; <?php //echo $user_email; ?>;
+        
+
+        alert("user id = " + user_id + "\nuser email = " + user_email);
 
         if (user_id == 2) 
         {
